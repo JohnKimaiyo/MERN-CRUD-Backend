277eec48b22530a446c1b9a1067a53a682a06b12
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-18 17:36:19 +0300

    final commt

--- routes/post.js
+++ routes/post.js
@@ -12,8 +12,23 @@ const postschema = new schema({
   postid: String,
 });
 
-router.get('/test',(req,res)=>{
-    res.send("Hllo Wordl using router")
+
+router.post('/addnewpost',(req,res)=>{
+const newpost = new PostModel({
+    title:req.body.title,
+    imageurl:req.body.imageurl,
+    description:req.body.description,
+    postid:req.body.postid
+})
+newpost.save(function(err){
+    if(err){
+        res.send('New Post added successfully')
+    }
+    else{
+        res.send(err)
+    }
+})
+   
 })
 const PostModel = mongoose.model("posts", postschema);
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const dbfile = require("../server/conn");
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:'true'}));
 const postroute = require("../server/routes/post");
 
 app.use("/api/post", postroute);
